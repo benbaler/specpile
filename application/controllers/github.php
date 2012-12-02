70dcc997582eb6e6357d28b2b878e6395646430d
Ben Baler <benbaler@gmail.com>
2012-12-02 02:47:20 +0200

    github

---
+++ application/controllers/github.php
@@ -0,0 +1,13 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Github extends CI_Controller {
+
+	public function index()
+	{
+		`git pull`;
+	}
+
+}
+
+/* End of file github.php */
+/* Location: ./application/controllers/github.php */
