f02fcf4da44b65128a9c12cf660df0c6d310a946
Ben Baler <benbaler@gmail.com>
2012-12-08 23:05:56 +0200

    'icecat'

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -23,7 +23,7 @@ class Home extends CI_Controller {
 		$this->load->view( 'header_v', $data );
 		$this->load->view( 'topbar_v', $this->_user() );
 		//$this->load->view( 'elements/categories_v', $data );
-		$this->load->view( 'elements/welcomeMsg_v');
+		$this->load->view( 'elements/welcomeMsg_v' );
 		$this->load->view( 'forms/search_v' );
 		$this->load->view( 'elements/results_v' );
 		$this->load->view( 'footer_v' );
@@ -156,8 +156,129 @@ class Home extends CI_Controller {
 	}
 
 	public function test3() {
-		$this->load->model('bing_m');
-		var_dump($this->bing_m->getPhotosByText('iphone 5'));
+		$this->load->model( 'bing_m' );
+		var_dump( $this->bing_m->getPhotosByText( 'iphone 5' ) );
+	}
+
+	public function test4(){
+		echo $this->_getICEcatProductSpecs('8808993518876', 1, 1);
+	}
+
+	private function _getICEcatProductSpecs( $ean, $drawdescription = 0, $drawpicture = 0 ) {
+		// Username and password for usage with ICEcat
+		$username = "benbaler";
+		$password = "benb1234";
+
+		// Return 0 and exit function if no EAN available
+		if ( $ean == null ) {
+			return 0;
+		}
+
+		// Get the product specifications in XML format
+		$context = stream_context_create( array(
+				'http' => array(
+					'header'  => "Authorization: Basic " . base64_encode( $username.":".$password )
+				)
+			) );
+		$data = file_get_contents( 'http://data.icecat.biz/xml_s3/xml_server3.cgi?ean_upc='.$ean.'&lang=en&output=productxml', false, $context );
+		$xml = new SimpleXMLElement( $data );
+
+		// Create arrays of item elements from the XML feed
+		$productPicture = $xml->xpath( "//Product" );
+		$productDescription = $xml->xpath( "//ProductDescription" );
+		$categories = $xml->xpath( "//CategoryFeatureGroup" );
+		$spec_items = $xml->xpath( "//ProductFeature" );
+
+		//Draw product specifications table if any specs available for the product
+		if ( $spec_items != null ) {
+			$categoryList = array();
+			foreach ( $categories as $categoryitem ) {
+				$catId = intval( $categoryitem->attributes() );
+				$titleXML = new SimpleXMLElement( $categoryitem->asXML() );
+				$title = $titleXML->xpath( "//Name" );
+				$catName = $title[0]->attributes();
+				//echo $catId . $catName['Value']. "<br />";
+				$categoryList[$catId] = $catName['Value'];
+			}
+
+			$specs =  "<table class='productspecs'>";
+			$i = 0;
+
+			$drawnCategories = array();
+
+			foreach ( $spec_items as $item ) {
+				$specValue = $item->attributes();
+				$titleXML = new SimpleXMLElement( $item->asXML() );
+				$title = $titleXML->xpath( "//Name" );
+				$specName = $title[0]->attributes();
+				$specCategoryId = intval( $specValue['CategoryFeatureGroup_ID'] );
+
+				if ( $specName['Value'] != "Source data-sheet" ) {
+					$class = $i%2==0?"odd":"even";
+					$specs .= "<tr class='".$class."'>
+						<td>
+							<table>";
+					if ( !in_array( $specCategoryId, $drawnCategories ) ) {
+						$specs .= "	<tr class='speccategory'>
+									<th><h3>".$categoryList[$specCategoryId]."</h3></th>
+								</tr>";
+						$drawnCategories[$i] = $specCategoryId;
+					}
+					$specs .= "		<tr>
+									<th>".utf8_decode( $specName['Value'] )."</th>
+								</tr>
+								<tr>
+									<td>";
+					if ( $specValue['Presentation_Value'] == "Y" ) {
+						$specs .= "Yes <img width='15' height='15' src='http://commons.hortipedia.com/images/thumb/3/35/Green_check_icon_file_16KB.png/120px-Green_check_icon_file_16KB.png' alt='Ja' />";
+					}
+					else if ( $specValue['Presentation_Value'] == "N" ) {
+							$specs .= "No <img width='15' height='15' src='http://commons.hortipedia.com/images/thumb/d/dd/Red_x_icon_file_18KB.png/120px-Red_x_icon_file_18KB.png' alt='Nee' />";
+						}
+					else {
+						$specs .= str_replace( '\n', '<br />', utf8_decode( $specValue['Presentation_Value'] ) );
+					}
+					$specs .= "</td>
+								</tr>
+							</table>
+						</td>
+					</tr>";
+				}
+				$i++;
+			}
+			$specs .= "</table>";
+
+			//Draw product description and link to manufacturer if available
+			if ( $drawdescription != 0 ) {
+				foreach ( $productDescription as $item ) {
+					$productValues = $item->attributes();
+					if ( $productValues['URL'] != null ) {
+						$specs .= "<p id='manufacturerlink'><a href='".$productValues['URL']."'>Productinformation from manufacturer</a></p>";
+					}
+					if ( $productValues['LongDesc'] != null ) {
+						$description = utf8_decode( str_replace( '\n', '', $productValues['LongDesc'] ) );
+						$description = str_replace( '<b>', '<strong>', $description );
+						$description = str_replace( '<B>', '<strong>', $description );
+						$description = str_replace( '</b>', '</strong>', $description );
+						$specs .= "<p id='manudescription'>".$description."</p>";
+					}
+				}
+			}
+
+			//Draw product picture if available
+			if ( $drawdescription != 0 ) {
+				foreach ( $productPicture as $item ) {
+					$productValues = $item->attributes();
+					if ( $productValues['HighPic'] != null ) {
+						$specs .= "<div id='manuprodpic'><img src='".$productValues['HighPic']."' alt='' /></div>";
+					}
+				}
+			}
+			return $specs;
+		}
+		else {
+			return 0;
+		}
 	}
 
 }
