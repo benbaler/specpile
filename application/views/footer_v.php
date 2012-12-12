7b4be808ee41583e00a05b02c535eb975c52b65e
Ben Baler <benbaler@gmail.com>
2012-12-12 17:23:48 +0200

    design

--- application/views/footer_v.php
+++ application/views/footer_v.php
@@ -3,11 +3,11 @@
 		<!-- <hr> -->
 		<br/>
 		<ul class="inline-list">
-			<li><a href="#">About</a></li>
+			<!-- <li><a href="#">About</a></li>
 			<li><a href="#">Jobs</a></li>
 			<li><a href="#">Privacy</a></li>
 			<li><a href="#">Terms</a></li>
-			<li><a href="#">Help</a></li>
+			<li><a href="#">Help</a></li> -->
 		</ul>
 	</div>
 </div>
