e84843fccced9d647765ea7c598aa8ed6046fd69
Ben Baler <benbaler@gmail.com>
2012-12-12 22:23:58 +0200

    icecat

--- application/views/elements/welcomeMsg_v.php
+++ application/views/elements/welcomeMsg_v.php
@@ -1,5 +1,5 @@
 <div class="row">
 <div class="twelve mobile-four columns">
-	<h1 class="subheader">Search for <b>cell phones</b>, <b>tablets</b> or <b>cameras</b>,<br/> get full specs.</h1>
+	<h1 class="subheader">Search for <b>cell phones</b>, <b>tablets</b> or <b>cameras</b><br/>and get full specs.</h1>
 </div>
 </div>
