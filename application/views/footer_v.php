9ac748afdfc466ee0653179f9c44f5280e2c0df9
Ben Baler <benbaler@gmail.com>
2012-12-12 17:40:51 +0200

    design

--- application/views/footer_v.php
+++ application/views/footer_v.php
@@ -12,5 +12,14 @@
 	</div>
 </div>
 
+	<script type="text/javascript">
+	var uvOptions = {};
+	(function() {
+		var uv = document.createElement('script'); uv.type = 'text/javascript'; uv.async = true;
+		uv.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'widget.uservoice.com/YK0wDSZnmiNAkEmIk7eg.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(uv, s);
+	})();
+	</script>
+	
 </body>
 </html>
--- application/views/header_v.php
+++ application/views/header_v.php
@@ -32,15 +32,6 @@
 	</script>
 
 	<script type="text/javascript">
-	var uvOptions = {};
-	(function() {
-		var uv = document.createElement('script'); uv.type = 'text/javascript'; uv.async = true;
-		uv.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'widget.uservoice.com/YK0wDSZnmiNAkEmIk7eg.js';
-		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(uv, s);
-	})();
-	</script>
-
-	<script type="text/javascript">
 
 	var _gaq = _gaq || [];
 	_gaq.push(['_setAccount', 'UA-36162016-1']);
