6c94f3e3ecd866ae9f415599852fb01f3721ba71
Ben Baler <benbaler@gmail.com>
2012-12-12 20:58:00 +0200

    design

--- application/views/elements/welcomeMsg_v.php
+++ application/views/elements/welcomeMsg_v.php
@@ -1,6 +1,5 @@
 <div class="row">
 <div class="twelve mobile-four columns">
-	<h1 class="subheader">Specpile.com</h1>
-	<h2 class="subheader">Search for Smartphones, get full specs.</h2>
+	<h1 class="subheader">Search for Smartphones,<br/> get full specs.</h1>
 </div>
 </div>
