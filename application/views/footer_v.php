a6664a3953f98d70b81d648531752a1525fde1fb
Ben Baler <benbaler@gmail.com>
2012-12-18 00:36:56 +0200

    favicon

--- application/views/footer_v.php
+++ application/views/footer_v.php
@@ -1,24 +1,27 @@
 <div class="row">
 	<div class="twelve columns last-column">
-		<!-- <hr> -->
-		<br/>
-		<ul class="inline-list">
-			<!-- <li><a href="#">About</a></li>
+<div class="row">
+	<div class="two centered columns">
+		<a href="mailto:team@specpile.com"><h6 class="subheader" style="font-size: 10px; color:#E0E0E0;">team@specpile.com</h6>
+		<!-- <ul class="inline-list">
+			<li><a href="#">About</a></li>
 			<li><a href="#">Jobs</a></li>
 			<li><a href="#">Privacy</a></li>
 			<li><a href="#">Terms</a></li>
-			<li><a href="#">Help</a></li> -->
-		</ul>
+			<li><a href="#">Help</a></li>
+		</ul> -->
 	</div>
+</div>	
+</div>
 </div>
 
 	<script type="text/javascript">
-	var uvOptions = {};
-	(function() {
-		var uv = document.createElement('script'); uv.type = 'text/javascript'; uv.async = true;
-		uv.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'widget.uservoice.com/YK0wDSZnmiNAkEmIk7eg.js';
-		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(uv, s);
-	})();
+	// var uvOptions = {};
+	// (function() {
+	// 	var uv = document.createElement('script'); uv.type = 'text/javascript'; uv.async = true;
+	// 	uv.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'widget.uservoice.com/YK0wDSZnmiNAkEmIk7eg.js';
+	// 	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(uv, s);
+	// })();
 	</script>
 	
 </body>
