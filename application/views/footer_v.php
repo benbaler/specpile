9216490390a942d62b57d4d9c0a53f0284c61e39
Ben Baler <benbaler@gmail.com>
2012-12-18 16:44:06 +0200

    inspectlet

--- application/views/footer_v.php
+++ application/views/footer_v.php
@@ -16,12 +16,12 @@
 </div>
 
 	<script type="text/javascript">
-	// var uvOptions = {};
-	// (function() {
-	// 	var uv = document.createElement('script'); uv.type = 'text/javascript'; uv.async = true;
-	// 	uv.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'widget.uservoice.com/YK0wDSZnmiNAkEmIk7eg.js';
-	// 	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(uv, s);
-	// })();
+	var uvOptions = {};
+	(function() {
+		var uv = document.createElement('script'); uv.type = 'text/javascript'; uv.async = true;
+		uv.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'widget.uservoice.com/YK0wDSZnmiNAkEmIk7eg.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(uv, s);
+	})();
 	</script>
 	
 </body>
