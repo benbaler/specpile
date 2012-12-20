b0a9f65aaa8cff4f79776ff016292ed3db93dc89
Ben Baler <benbaler@gmail.com>
2012-12-20 16:03:18 +0200

    img

--- application/views/elements/welcomeMsg_v.php
+++ application/views/elements/welcomeMsg_v.php
@@ -13,7 +13,7 @@
 			</style>
 			<div class="six columns offset-by-one">
 				<img src="/assets/images/logo.png"/>
-				<h1 class="subheader"><a href="/" style="color:black"><b>Search</b></a> and <a href="/product/compare" style="color:black"><b>Compare</b></a> <b>Specifications</b> of <a href="/product/smartphones"><b>Smartphones</b></a>, <a href="/product/tablets"><b>Tablets</b></a> and <a href="/product/cameras"><b>Cameras</b></a></h1>
+				<h1 class="subheader"><a href="/" style="color:black"><b>Search</b></a> and <a href="/product/compare" style="color:black"><b>Compare</b></a> <b>Specifications</b> of <a href="/product/smartphones"><b>Smart Phones</b></a>, <a href="/product/tablets"><b>Tablets</b></a> and <a href="/product/cameras"><b>Digital Cameras</b></a></h1>
 			</div>
 
 			<div class="five columns">
