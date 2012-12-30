0442f60ef663cdd086aa7bcd89b3d70e507211de
Ben Baler <benbaler@gmail.com>
2012-12-30 03:18:52 +0200

    changes

--- application/views/elements/results_v.php
+++ application/views/elements/results_v.php
@@ -6,33 +6,33 @@
 
 <div class="row">
 	<div class="twelve columns">
-			<ul id="results-panel" class="block-grid four-up mobile-two-up">
-				<li>Search to get results!</li>
-			</ul>
+		<ul id="results-panel" class="block-grid four-up mobile-two-up">
+			<li>Search to get results!</li>
+		</ul>
 	</div>
 </div>
 
 <script type="text/template" id="product-template">
 <a href="/product/view/<%= _id %>">
 
-<div class="row search-result">
-<div class="twelve mobile-four columns product-result">
+	<div class="row search-result">
+		<div class="twelve mobile-four columns product-result">
 
-<div class="row">
-<div class="twelve mobile-four columns result-img">
-<img src="<% if(image != '') { %> <%= image %> <% } else { %> assets/images/unknown.png  <% } %>" class="productImg"/>
-</div>
-</div>
+			<div class="row">
+				<div class="twelve mobile-four columns result-img">
+				<img src="<% if(image != '') { %> <%= image %> <% } else { %> assets/images/unknown.png  <% } %>" class="productImg"/>
+				</div>
+			</div>
 
-<div class="row">
-<div class="twelve mobile-four columns result-name">
-<%= category_name %> &rsaquo; <%= brand_name %><br/><b><%= name %><b/>
-</div>
-</div>
+			<div class="row">
+				<div class="twelve mobile-four columns result-name">
+				<%= category_name %> &rsaquo; <%= brand_name %><br/><b><%= name %></b>
+				</div>
+			</div>
 
 
-</div>
-</div>
+		</div>
+	</div>
 
 </a>
 </script>
