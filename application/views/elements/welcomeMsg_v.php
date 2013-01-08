b761a00353ad5daa651ad1b9f75d846c3fe02790
Ben Baler <benbaler@gmail.com>
2013-01-08 05:13:37 +0200

    update

--- application/views/elements/welcomeMsg_v.php
+++ application/views/elements/welcomeMsg_v.php
@@ -6,6 +6,7 @@
 				<h1 class="subheader"><a href="/" style="color:black"><b>Search</b></a> & <a href="/product/compare" style="color:black"><b>Compare</b></a> <b>Specifications</b> of <a href="/product/smartphones"><b>Smart Phones</b></a>, <a href="/product/tablets"><b>Tablets</b></a> & <a href="/product/cameras"><b>Digital Cameras</b></a></h1>
 				<div style="text-align:center;">
 					<a href="https://play.google.com/store/apps/details?id=com.ben.com" target="_blank"><img src="/assets/images/android.png" width="118"/></a>
+					<span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
 					<a href="https://itunes.apple.com/il/app/specpile-search-compare-products/id590381093?mt=8" target="_blank"><img src="/assets/images/Download_on_the_App_Store_Badge_US-UK_135x40.png"/></a>
 				</div>
 			</div>
