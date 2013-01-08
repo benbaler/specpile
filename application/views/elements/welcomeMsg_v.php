d988f1a09c5a1dac574afd637485390f4deb7a8c
Ben Baler <benbaler@gmail.com>
2013-01-08 04:11:16 +0200

    update

--- application/views/elements/welcomeMsg_v.php
+++ application/views/elements/welcomeMsg_v.php
@@ -6,7 +6,7 @@
 				<h1 class="subheader"><a href="/" style="color:black"><b>Search</b></a> & <a href="/product/compare" style="color:black"><b>Compare</b></a> <b>Specifications</b> of <a href="/product/smartphones"><b>Smart Phones</b></a>, <a href="/product/tablets"><b>Tablets</b></a> & <a href="/product/cameras"><b>Digital Cameras</b></a></h1>
 			</div>
 			<div class="five columns">
-				<iframe src="/home/slide" scrolling="no" width="300" height="600" frameborder="0"></iframe>
+				<iframe src="/home/slide" scrolling="no" width="300" height="555" frameborder="0"></iframe>
 			</div>
 		</div>
 	</div>
