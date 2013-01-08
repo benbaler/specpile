3b5abc2c9f00c0cd397c741992b731ba23793c28
Ben Baler <benbaler@gmail.com>
2013-01-08 04:09:22 +0200

    update

--- application/views/elements/welcomeMsg_v.php
+++ application/views/elements/welcomeMsg_v.php
@@ -3,7 +3,7 @@
 		<div class="row">
 			<div class="six columns offset-by-one">
 				<img src="/assets/images/logo.png"/>
-				<h1 class="subheader"><a href="/" style="color:black"><b>Search</b></a> and <a href="/product/compare" style="color:black"><b>Compare</b></a> <b>Specifications</b> of <a href="/product/smartphones"><b>Smart Phones</b></a>, <a href="/product/tablets"><b>Tablets</b></a> and <a href="/product/cameras"><b>Digital Cameras</b></a></h1>
+				<h1 class="subheader"><a href="/" style="color:black"><b>Search</b></a> & <a href="/product/compare" style="color:black"><b>Compare</b></a> <b>Specifications</b> of <a href="/product/smartphones"><b>Smart Phones</b></a>, <a href="/product/tablets"><b>Tablets</b></a> & <a href="/product/cameras"><b>Digital Cameras</b></a></h1>
 			</div>
 			<div class="five columns">
 				<iframe src="/home/slide" scrolling="no" width="300" height="600" frameborder="0"></iframe>
