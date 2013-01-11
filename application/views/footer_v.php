2b3b7f85746edd1d2633b1ec4a755c681629f274
Ben Baler <benbaler@gmail.com>
2013-01-11 03:47:12 +0200

    orange

--- application/views/footer_v.php
+++ application/views/footer_v.php
@@ -1,22 +1,18 @@
-<!-- <div class="row hide-for-medium-down">
+<div class="row show-for-medium-down">
 	<div class="twelve columns centered">
 		<ul class="block-grid two-up mobile-two-up">
 			<li style="text-align:right;"><a href="https://play.google.com/store/apps/details?id=com.ben.com" target="_blank"><img src="/assets/images/android.png" width="118"/></a></li>
 			<li><a href="https://itunes.apple.com/il/app/specpile-search-compare-products/id590381093?mt=8" target="_blank"><img src="/assets/images/Download_on_the_App_Store_Badge_US-UK_135x40.png"/></a></li>
 		</ul>
 	</div>
-</div> -->
+</div>
 
 <div class="row">
 	<div class="twelve columns centered">
-		<div class="row">
-			<div class="two columns mobile-four centered">
-				<ul class="inline-list">
-					<li><a href="mailto:team@specpile.com" target="_blank">contact</a></li>
-					<li><a href="http://blog.specpile.com" target="_blank">blog</a></li>
-				</ul>
-			</div>
-		</div>
+		<ul class="block-grid two-up mobile-two-up">
+			<li style="text-align:right;"><a href="mailto:team@specpile.com" target="_blank">contact</a></li>
+			<li><a href="http://blog.specpile.com" target="_blank">blog</a></li>
+		</ul>
 	</div>
 </div>
 
@@ -29,21 +25,21 @@
 	</div>
 </div>
 
-	
-	<script type="text/javascript">
-	function googleTranslateElementInit() {
-		new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE, autoDisplay: false, gaTrack: true, gaId: 'UA-36162016-1'}, 'google_translate_element');
-	}
-	</script>
-	<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
 
-	<script type="text/javascript">
-	(function() {
-		var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-		po.src = 'https://apis.google.com/js/plusone.js';
-		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-	})();
-	</script>
+<script type="text/javascript">
+function googleTranslateElementInit() {
+	new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE, autoDisplay: false, gaTrack: true, gaId: 'UA-36162016-1'}, 'google_translate_element');
+}
+</script>
+<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+
+<script type="text/javascript">
+(function() {
+	var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+	po.src = 'https://apis.google.com/js/plusone.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+})();
+</script>
 
 </body>
 </html>
